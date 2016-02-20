8e37f3af672426aa522f421bb02c661b1c3dfc82
mark <email>
2016-02-20 10:13:08 +0100

    add antrun-set-maven-props project

---
+++ antrun-set-maven-props/src/test/java/com/github/jjYBdx4IL/maven/examples/antrun/AntRunPropertyInjectionTest.java
@@ -0,0 +1,20 @@
+package com.github.jjYBdx4IL.maven.examples.antrun;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author jjYBdx4IL
+ */
+public class AntRunPropertyInjectionTest {
+
+    @Test
+    public void test() throws IOException {
+        assertEquals("<replaced> NOT replACED\n",
+                IOUtils.toString(getClass().getResource("testfile.txt"), "ASCII"));
+    }
+}
