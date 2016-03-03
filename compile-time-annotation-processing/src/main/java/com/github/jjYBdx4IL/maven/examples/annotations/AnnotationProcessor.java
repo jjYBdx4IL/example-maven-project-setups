c394adc8176332a782b56a930870af28342427b3
jjYBdx4IL <jjYBdx4IL@github.com>
2016-03-03 22:15:00 +0100

    better annotation processor example

--- compile-time-annotation-processing/src/main/java/com/github/jjYBdx4IL/maven/examples/annotations/AnnotationProcessor.java
+++ compile-time-annotation-processing/src/main/java/com/github/jjYBdx4IL/maven/examples/annotations/AnnotationProcessor.java
@@ -12,19 +12,25 @@ import com.helger.jcodemodel.writer.FileCodeWriter;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.lang.annotation.AnnotationFormatError;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.QualifiedNameable;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,14 +39,38 @@ import org.slf4j.LoggerFactory;
  *
  * @author jjYBdx4IL
  */
-@SupportedAnnotationTypes("com.github.jjYBdx4IL.maven.examples.annotations.ExampleAnnotation")
-@SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class AnnotationProcessor extends AbstractProcessor {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationProcessor.class);
 
+    private Types typeUtils;
+    private Elements elementUtils;
+    private Filer filer;
+    private Messager messager;
+
     public AnnotationProcessor() {
         super();
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        typeUtils = processingEnv.getTypeUtils();
+        elementUtils = processingEnv.getElementUtils();
+        filer = processingEnv.getFiler();
+        messager = processingEnv.getMessager();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> annotataions = new LinkedHashSet<>();
+        annotataions.add(ExampleAnnotation.class.getCanonicalName());
+        return annotataions;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
     }
 
     @Override
@@ -49,7 +79,7 @@ public class AnnotationProcessor extends AbstractProcessor {
             return true;
         }
 
-        List<String> classesFound = new ArrayList<>();
+        List<Element> classesFound = new ArrayList<>();
 
         for (Element elem : roundEnv.getElementsAnnotatedWith(ExampleAnnotation.class)) {
             log.info("annotation found in " + elem.toString());
@@ -60,9 +90,12 @@ public class AnnotationProcessor extends AbstractProcessor {
 
             ExampleAnnotation exampleAnno = elem.getAnnotation(ExampleAnnotation.class);
 
-            // ...
+            TypeMirror requiredIface = elementUtils.getTypeElement(Serializable.class.getCanonicalName()).asType();
+            if (typeUtils.isAssignable(requiredIface, elem.asType())) {
+                log.info("annotated class implements Serializable");
+            }
 
-            classesFound.add(elem.toString());
+            classesFound.add(elem);
         }
 
         try {
@@ -74,7 +107,7 @@ public class AnnotationProcessor extends AbstractProcessor {
         return true; // no further processing of this annotation type
     }
 
-    private void writeSourceCode(List<String> classesFound) throws JClassAlreadyExistsException, IOException {
+    private void writeSourceCode(List<Element> classesFound) throws JClassAlreadyExistsException, IOException {
         JCodeModel cm = new JCodeModel();
         JDefinedClass cls = cm._class(getClass().getPackage().getName() + ".GeneratedByAnnotationProcessor");
 
@@ -82,7 +115,7 @@ public class AnnotationProcessor extends AbstractProcessor {
         toStringMethod.annotate(Override.class);
         AbstractJClass stringBuilder = cm.ref(StringBuilder.class);
         JVar sbVar = toStringMethod.body().decl(stringBuilder, "sb", JExpr._new(stringBuilder));
-        for (String className : classesFound) {
+        for (Element className : classesFound) {
             toStringMethod.body().invoke(sbVar, "append").arg(className + " ");
         }
         toStringMethod.body()._return(sbVar.invoke("toString"));
--- compile-time-annotation-processing/src/main/java/com/github/jjYBdx4IL/maven/examples/annotations/ExampleImplementation1.java
+++ compile-time-annotation-processing/src/main/java/com/github/jjYBdx4IL/maven/examples/annotations/ExampleImplementation1.java
@@ -1,10 +1,12 @@
 package com.github.jjYBdx4IL.maven.examples.annotations;
 
+import java.io.Serializable;
+
 /**
  *
  * @author jjYBdx4IL
  */
 @ExampleAnnotation
-public class ExampleImplementation1 {
+public class ExampleImplementation1 implements Serializable {
 
 }
