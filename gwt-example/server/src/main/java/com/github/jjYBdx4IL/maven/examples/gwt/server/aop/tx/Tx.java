356557a6ad22f516e7b657a8cca384b5b3052cee
jjYBdx4IL <jjYBdx4IL@github.com>
2017-06-02 08:23:03 +0200

    unfinished TxManager

---
+++ gwt-example/server/src/main/java/com/github/jjYBdx4IL/maven/examples/gwt/server/aop/tx/Tx.java
@@ -0,0 +1,10 @@
+package com.github.jjYBdx4IL.maven.examples.gwt.server.aop.tx;
+
+/**
+ * Annotation to indicate servlet methods to support with transaction handling.
+ * 
+ * @author jjYBdx4IL
+ */
+public @interface Tx {
+
+}
