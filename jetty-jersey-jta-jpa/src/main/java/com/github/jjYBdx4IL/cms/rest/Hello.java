7892cb18883bc0f337c0d54d0b1ded1b208faeb4
jjYBdx4IL <jjYBdx4IL@github.com>
2017-08-21 22:46:26 +0200

    jetty jersey jta example running, without jta yet

---
+++ jetty-jersey-jta-jpa/src/main/java/com/github/jjYBdx4IL/cms/rest/Hello.java
@@ -0,0 +1,36 @@
+package com.github.jjYBdx4IL.cms.rest;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+@Singleton // needed to avoid entity manager leak with TX manager because the TX
+           // manager assumes GenericServlet's destroy method for cleanup
+public class Hello {
+
+    // This method is called if TEXT_PLAIN is request
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello Jersey";
+    }
+
+    // This method is called if XML is request
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayXMLHello() {
+        return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+    }
+
+    // This method is called if HTML is request
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHtmlHello() {
+        return "<html> " + "<title>" + "Hello Jersey" + "</title>" + "<body><h1>" + "Hello Jersey" + "</body></h1>"
+                + "</html> ";
+    }
+
+}
